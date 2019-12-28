ab5824ee44853f30ac600a3facb03c2808ebd8c0
Cedric <cedric.wilting@stud.uni-due.de>
2019-12-28 14:26:21 +0100

    adapt to felix's api

--- spotify_remote.py
+++ spotify_remote.py
@@ -1,43 +1,48 @@
 import spotipy
 import spotipy.util as util
-import spotipy.oauth2 as oauth2
+import spotipy.auth as auth
 
 
 class SpotifyRemote():
     def __init__(self, client_id, client_secret, username):
-        auth = oauth2.SpotifyClientCredentials(
-            client_id = client_id,
-            client_secret = client_secret,
-        )
+        self.spotify = self.setup_spotify(client_id, client_secret)
+        self.spotify_client = self.setup_spotify_client(client_id, client_secret, username)
 
-        token = auth.get_access_token()
-        self.spotify = spotipy.Spotify(auth=token)
-
-        scope = "user-library-read"
-        token = util.prompt_for_user_token(
-            username,
-            scope,
+    def setup_spotify(self, client_id, client_secret):
+        cred = auth.Credentials(
             client_id = client_id,
             client_secret = client_secret,
             redirect_uri = "http://localhost:/callback",
         )
-        self.spotify_user = spotipy.Spotify(auth=token)
+        token = cred.request_client_token()
+        return spotipy.Spotify(token=token)
+
+
+    def setup_spotify_client(self, client_id, client_secret, username):
+         scope = "user-modify-playback-state"#"user-library-read"
+         token = util.prompt_for_user_token(
+             scope = scope,
+             client_id = client_id,
+             client_secret = client_secret,
+             redirect_uri = "http://localhost:/callback",
+         )
+         return spotipy.client.Spotify(token=token)
 
 
     def search_track(self, search_string):
         results = {}
-        for track in self.spotify.search(search_string, type="track")["tracks"]["items"]:
-            results[self.title_from_track(track)] = track["id"]
+        for track in self.spotify.search(search_string)[0].items:
+            results[self.title_from_track(track)] = track.id
         return results
 
     def title_from_track(self, track):
-        artists = ", ".join([artist["name"] for artist in track["artists"]])
-        trackname = track["name"]
+        artists = ", ".join([artist.name for artist in track.artists])
+        trackname = track.name
         return f"{artists} - {trackname}"
 
 
     def get_track_preview(self, id):
-        return self.spotify.track(id)["preview_url"]
+        return self.spotify.track(id).preview_url
 
 
     def now_playing(self):
